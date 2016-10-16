90053ea91849d91a89310b395cd858652ac38d81
Sean M Hofer <seanhofer8@gmail.com>
2016-10-16 04:24:11 -0400

    remove redundant icon/button

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,21 @@
 var obj = new Object();
 var track = 0;
+
 function submit(e) {
   obj.category = document.getElementById('category').value;
   obj.description = document.getElementById('description').value;
   submission(JSON.stringify(obj));
 }
 
-function about(e){
-  alert("Copyright 2016 etc.\nExtension by Sean Hofer\nPage by Michael Atanasio and Zach Shea\nBackend by Jake Ferrante");
-
-}
-
-function settings(e){
-  if(track == 0){
-    document.getElementById("main").setAttribute("style","visibility:hidden");
-    document.getElementById("settingMenu").setAttribute("style","visibility:visible");
+function settings(e) {
+  if (track == 0) {
+    document.getElementById("main").setAttribute("style", "visibility:hidden");
+    document.getElementById("settingMenu").setAttribute("style", "visibility:visible");
     document.getElementById("settingMenu").style.height = "auto";
     track = 1;
-  }
-  else{
-    document.getElementById("main").setAttribute("style","visibility:visible");
-    document.getElementById("settingMenu").setAttribute("style","visibility:hidden");
+  } else {
+    document.getElementById("main").setAttribute("style", "visibility:visible");
+    document.getElementById("settingMenu").setAttribute("style", "visibility:hidden");
     track = 0;
   }
 }
@@ -35,7 +30,6 @@ document.addEventListener('DOMContentLoaded', function() {
     obj.status = 0;
   });
   document.getElementById('submit').addEventListener('click', submit);
-  document.getElementById('about').addEventListener('click', about);
   document.getElementById('settings').addEventListener('click', settings);
 });
 
