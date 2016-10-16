80619a300f065264c93f2d13175b158a50e9737c
Sean M Hofer <seanhofer8@gmail.com>
2016-10-16 01:49:15 -0400

    icons added, visual tweaks

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,28 @@
 var obj = new Object();
-
-function clickHandler(e) {
+var track = 0;
+function submit(e) {
   obj.category = document.getElementById('category').value;
   obj.description = document.getElementById('description').value;
   submission(JSON.stringify(obj));
+}
+
+function about(e){
+  alert("Copyright 2016 etc.\nExtension by Sean Hofer\nPage by Michael Atanasio and Zach Shea\nBackend by Jake Ferrante");
+
+}
+
+function settings(e){
+  if(track == 0){
+    document.getElementById("main").setAttribute("style","visibility:hidden");
+    document.getElementById("settingMenu").setAttribute("style","visibility:visible");
+    document.getElementById("settingMenu").style.height = "auto";
+    track = 1;
+  }
+  else{
+    document.getElementById("main").setAttribute("style","visibility:visible");
+    document.getElementById("settingMenu").setAttribute("style","visibility:hidden");
+    track = 0;
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -17,11 +36,13 @@ document.addEventListener('DOMContentLoaded', function() {
     obj.image = dataUrl;
     obj.status = 0;
   });
-  document.getElementById('submit').addEventListener('click', clickHandler);
+  document.getElementById('submit').addEventListener('click', submit);
+  document.getElementById('about').addEventListener('click', about);
+  document.getElementById('settings').addEventListener('click', settings);
 });
 
 function submission(object) {
-  var url = "http://bully-report-system.azurewebsites.net/";
+  var url = "http://bully-report-system.azurewebsites.net/api/ticket/submit";
   var params = object;
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
