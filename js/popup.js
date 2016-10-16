72f3e080b56a6389a1621e5aa7f1ad58f8cb4b11
Sean M Hofer <seanhofer8@gmail.com>
2016-10-15 23:57:16 -0400

    function still needs adjustment

--- js/popup.js
+++ js/popup.js
@@ -3,19 +3,34 @@ var obj = new Object();
 function clickHandler(e) {
   obj.category = document.getElementById('category').value;
   obj.description = document.getElementById('description').value;
-  JSON.stringify(obj);
+  submission(JSON.stringify(obj));
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.captureVisibleTab(null, function(dataUrl) {
     var currentDate = new Date();
     chrome.tabs.getSelected(null, function(tab) {
       obj.url = tab.url;
     });
-    obj.name = "Sean Hofer";
     obj.timestamp = ((currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear() + " @ " + currentDate.getHours() + ":" + currentDate.getMinutes() + currentDate.getSeconds());
     obj.email = "seansmail@email.fake";
     obj.image = dataUrl;
+    obj.status = 0;
   });
-    document.getElementById('submit').addEventListener('click', clickHandler);
+  document.getElementById('submit').addEventListener('click', clickHandler);
 });
+
+function submission(object) {
+  var url = "http://bully-report-system.azurewebsites.net/";
+  var params = object;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      alert("Thanks! Your report has been processed.");
+    }
+  }
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send(params);
+
+}
